89848018383c586f4cba58aebcec1028ec2c6e48
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-06 15:28:12 -0400

    Update travis_ebook_converters.sh

--- script/travis_ebook_converters.sh
+++ script/travis_ebook_converters.sh
@@ -4,7 +4,7 @@ set -ex
 apt-get update -qq
 
 # PDF
-apt-get install -qq xfonts-75dpi xfonts-base gdebi-core
+apt-get install -qq xfonts-75dpi xfonts-base gdebi-core libmysqlclient-dev
 wget http://security.ubuntu.com/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1.1_amd64.deb
 gdebi --n ./libpng12-0_1.2.54-1ubuntu1.1_amd64.deb
 wget https://downloads.wkhtmltopdf.org/0.12/0.12.5/wkhtmltox_0.12.5-1.xenial_amd64.deb
