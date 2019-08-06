11984443f21e84e616f4a81ae789f882b26fd5c3
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-06 16:19:10 +0000

    initial config

---
+++ yourbased.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -ex
+
+bash script/travis_ebook_converters.sh
+bundle install --jobs=3 --retry=3
+bash script/travis_configure.sh
+bash script/travis_elasticsearch_upgrade.sh
+bash script/travis_multiple_redis.sh
+bash script/travis_mysql.sh
+RAILS_ENV=test bundle exec rake db:schema:load --trace
+RAILS_ENV=test bundle exec rake db:migrate --trace
+bundle exec $TEST_GROUP
