283eb2bbe609b2957ec8fbed0fd4b086bd6e5f1f
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-06 13:15:53 -0400

    Update travis_ebook_converters.sh

--- script/travis_ebook_converters.sh
+++ script/travis_ebook_converters.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ex
 
 apt-get update -qq
 
@@ -8,7 +8,7 @@ apt-get install -qq xfonts-75dpi xfonts-base gdebi-core
 wget http://security.ubuntu.com/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1.1_amd64.deb
 gdebi --n ./libpng12-0_1.2.54-1ubuntu1.1_amd64.deb
 wget https://downloads.wkhtmltopdf.org/0.12/0.12.5/wkhtmltox_0.12.5-1.xenial_amd64.deb
-gdebi ./wkhtmltox_0.12.5-1.xenial_amd64.deb
+gdebi --n ./wkhtmltox_0.12.5-1.xenial_amd64.deb
 wkhtmltopdf --version
 
 # Calibre
