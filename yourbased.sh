bf7c6282eeff3f4db6a80089670c9199665ebbf4
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-08-13 19:35:12 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -4,7 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 bash script/travis_ebook_converters.sh
 bundle install --jobs=3 --retry=3
 bash script/travis_configure.sh
-bash script/travis_elasticsearch_upgrade.sh
+# bash script/travis_elasticsearch_upgrade.sh
 bash script/travis_multiple_redis.sh
 bash script/travis_mysql.sh
 RAILS_ENV=test bundle exec rake db:schema:load --trace
